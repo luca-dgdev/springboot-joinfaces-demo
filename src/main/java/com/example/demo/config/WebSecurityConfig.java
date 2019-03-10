bdaa6a6ab18e7eb05f51be268cafddc4ce4fdb75
luca-dgdev <luca.dgdev@gmail.com>
2019-03-10 18:57:27 +0100

    disable Spring Security

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,53 +1,53 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
-	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-		
-        http
-            .authorizeRequests().antMatchers("/", "/index.xhtml").permitAll()
-            .and()
-            .authorizeRequests().antMatchers("/pagina1*").access("hasRole('ROLE1')")
-            .and()
-            .authorizeRequests().antMatchers("/pagina2*").access("hasRole('ROLE2')")
-            .and()
-            .formLogin().permitAll()
-            .and()
-            .logout().permitAll()
-            .and().exceptionHandling().accessDeniedPage("/403.xhtml");
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user1 =
-             User.withDefaultPasswordEncoder()
-                .username("user1")
-                .password("password")
-                .roles("ROLE1")
-                .build();
-        
-        UserDetails user2 =
-                User.withDefaultPasswordEncoder()
-                   .username("user2")
-                   .password("password")
-                   .roles("ROLE2")
-                   .build();
-
-		return new InMemoryUserDetailsManager(user1, user2);
-	}
-}
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.access.AccessDecisionManager;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    
+//	@Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//		
+//        http
+//            .authorizeRequests().antMatchers("/", "/index.xhtml").permitAll()
+//            .and()
+//            .authorizeRequests().antMatchers("/pagina1*").access("hasRole('ROLE1')")
+//            .and()
+//            .authorizeRequests().antMatchers("/pagina2*").access("hasRole('ROLE2')")
+//            .and()
+//            .formLogin().permitAll()
+//            .and()
+//            .logout().permitAll()
+//            .and().exceptionHandling().accessDeniedPage("/403.xhtml");
+//    }
+//
+//    @Bean
+//    @Override
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user1 =
+//             User.withDefaultPasswordEncoder()
+//                .username("user1")
+//                .password("password")
+//                .roles("ROLE1")
+//                .build();
+//        
+//        UserDetails user2 =
+//                User.withDefaultPasswordEncoder()
+//                   .username("user2")
+//                   .password("password")
+//                   .roles("ROLE2")
+//                   .build();
+//
+//		return new InMemoryUserDetailsManager(user1, user2);
+//	}
+//}
